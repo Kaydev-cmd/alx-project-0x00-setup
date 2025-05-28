3cfdc639d22d063195e4c9d8a4d155aa2ebe0ab8
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-28 14:18:30 +0200

    feat: add Button component with rounded-lg style

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -13,6 +13,7 @@ const Landing: React.FC = () => {
       <div className="flex justify-around items-center mt-4">
         <Button title="Small Rounded-sm" styles="text-sm rounded-sm" />
         <Button title="Medium Rounded-md" styles="text-base rounded-md" />
+        <Button title="Large Rounded-lg" styles="text-lg rounded-lg" />
         <Button title="Large Rounded-full" styles="text-lg rounded-full" />
       </div>
     </div>
