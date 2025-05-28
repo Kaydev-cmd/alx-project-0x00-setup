d1ef26ce3f182faeb9f4285253c477f4a49fcdc8
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-28 09:45:20 +0200

    feat: add listingtitle, location, and rating with star icon

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -19,6 +19,20 @@ const Card: React.FC = () => {
         <Pill title="Self CheckIn" />
         <Pill title="Free Reschedule" />
       </div>
+      <div className="flex items-center justify-between">
+        <div>
+          <h3 className="font-semibold text-[22px]">
+            Villa Arrecife Beach House
+          </h3>
+          <p className="font-medium text-[17px] text-[#929292]">
+            Sideman, Bali, Indonesia
+          </p>
+        </div>
+        <div className="flex items-center">
+          <Image src={STAR_IMAGE} alt="Star" />
+          <p className="font-medium text-[17px] ml-2">4.76</p>
+        </div>
+      </div>
     </div>
   );
 };
