faae7d5e25edfa127115e50ae90c010930d8eb51
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-28 09:31:18 +0200

    feat: set up initial Card component with image imports and Pill component

---
+++ alx-project-0x00/components/Card.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import HOUSE_IMAGE from "@/public/assets/images/house.jpg";
+import STAR_IMAGE from "@/public/assets/images/star.png";
+import React from "react";
+import Pill from "./Pill";
+
+const Card: React.FC = () => {
+  return <div></div>;
+};
+
+export default Card;
