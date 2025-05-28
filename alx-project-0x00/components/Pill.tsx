f43067bd824d8cfc87b29bea825ec688ce1f5438
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-28 10:17:21 +0200

    feat: create reusable Pill component for displaying tag-like UI elements

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pill: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center bg-[#f9f9f9] px-3 w-auto h-[27px] rounded-full">
+      <p className="text-sm">Title</p>
+    </div>
+  );
+};
+
+export default Pill;
