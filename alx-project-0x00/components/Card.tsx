528c8e98af7c031d1ef36a95b46ad7d806ef5ddc
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-28 09:37:04 +0200

    feat: add hover effects and cursor pointer to house image container

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -5,7 +5,17 @@ import React from "react";
 import Pill from "./Pill";
 
 const Card: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg">
+      <Image
+        className="rounded-lg"
+        src={HOUSE_IMAGE}
+        width={378.56}
+        height={299.37}
+        alt="House image"
+      />
+    </div>
+  );
 };
 
 export default Card;
