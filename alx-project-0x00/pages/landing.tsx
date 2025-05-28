6a1641ea41ce311aa6059e21b7efa29ff78bc2e7
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-28 11:52:45 +0200

    feat: import and add Card components in a flex layout with centered alignment

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,14 @@
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
   return (
     <div className="text-xl font-extralight">
       <h1>Landing Page</h1>
+      <div className="flex justify-around items-center">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
